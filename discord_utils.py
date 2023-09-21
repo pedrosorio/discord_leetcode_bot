d20a4126e113a929d506bae0f1404b419b8f13a4
Pedro Osorio <mebm.pedrosorio@gmail.com>
2023-09-21 08:30:05 -0700

    Add edit_message to discord_utils.py
    Used this to edit the suffix of the most recent message as
    requested by a discord server owner. May come in handy later.

--- discord_utils.py
+++ discord_utils.py
@@ -2,7 +2,8 @@ import json
 import requests
 
 DISCORD_BASE_URL = 'https://discord.com/api' 
-DISCORD_MESSAGE_URL = DISCORD_BASE_URL + '/channels/{channel_id}/messages'
+DISCORD_MESSAGES_URL = DISCORD_BASE_URL + '/channels/{channel_id}/messages'
+DISCORD_MESSAGE_URL = DISCORD_MESSAGES_URL + '/{message_id}' 
 DISCORD_USER_URL = DISCORD_BASE_URL + '/users/{id}'
 DISCORD_CHANNEL_URL = DISCORD_BASE_URL + '/channels/{id}'
 DISCORD_GUILD_URL = DISCORD_BASE_URL + '/guilds/{id}'
@@ -31,12 +32,21 @@ def get_guild(guild_id):
     return get_discord_resource(DISCORD_GUILD_URL, guild_id)
 
 def post_message(message, channel_id=TEST_CHANNEL_ID):
-    response = requests.post(url=DISCORD_MESSAGE_URL.format(channel_id=channel_id), headers=DISCORD_AUTH_HEADER, json={'content': message})
+    response = requests.post(url=DISCORD_MESSAGES_URL.format(channel_id=channel_id), headers=DISCORD_AUTH_HEADER, json={'content': message})
     print(response.status_code)
     print(response.content)
 
+def get_message(message_id, channel_id=TEST_CHANNEL_ID):
+    response = requests.get(url=DISCORD_MESSAGE_URL.format(channel_id=channel_id, message_id=message_id), headers=DISCORD_AUTH_HEADER)
+    print(response.status_code)
+    return response.content
+
+def edit_message(new_content, message_id, channel_id=TEST_CHANNEL_ID):
+    response = requests.patch(url=DISCORD_MESSAGE_URL.format(channel_id=channel_id, message_id=message_id), headers=DISCORD_AUTH_HEADER, json={'content': new_content})
+    print(response.status_code)
+    return response.content
 
 def delete_message(channel_id, message_id):
-    response = requests.delete(url=DISCORD_MESSAGE_URL.format(channel_id=channel_id)+f'/{message_id}', headers=DISCORD_AUTH_HEADER)
+    response = requests.delete(url=DISCORD_MESSAGE_URL.format(channel_id=channel_id, message_id=message_id), headers=DISCORD_AUTH_HEADER)
     print(response.status_code)
     print(response.content)
